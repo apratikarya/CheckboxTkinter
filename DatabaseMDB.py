d1267f84a048e5eaf185ee7051a589d5f14fedfa
apratikarya <you@example.com>
2022-11-16 10:12:50 +0530

    MDatabase

--- DatabaseMDB.py
+++ DatabaseMDB.py
@@ -4,14 +4,13 @@ from tkinter import *
 root = Tk()
 root.geometry("300x300")
 client = MongoClient("localhost", 27017)
-
+# db
+database = client["student"]
+# collection
+col = database["student_info"]
 
 # insert
 def HandleInsert():
-    # db
-    database = client["student"]
-    # collection
-    col = database["student_info"]
     name = e1.get()
     col.insert_one({'name': name})
     e1.delete(0, END)
@@ -19,22 +18,16 @@ def HandleInsert():
 
 # to get values from collection
 def HandleGet():
-    database = client["student"]
-    col = database["student_info"]
     data=col.find()
     for i in data:
         print(i)
 
 # to delete values from
 def HandleDelete():
-    database = client["student"]
-    col = database["student_info"]
     col.delete_one({'name':'Kiran'})
     print("Deleted Sucessfully...")
 
 def HandleUpdate():
-    database = client["student"]
-    col = database["student_info"]
     col.update_one({'name':'Karan'},{'$set':{'name':'Zyz'}})
 
 l = Label(root, text="Enter Name")
